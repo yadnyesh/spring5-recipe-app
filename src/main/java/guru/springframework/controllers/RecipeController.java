6823f6bab7432cb464e937ce7dd5048da341f05b
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2019-08-17 14:42:12 +0530

    207. Delete a Recipe

--- src/main/java/guru/springframework/controllers/RecipeController.java
+++ src/main/java/guru/springframework/controllers/RecipeController.java
@@ -16,6 +16,7 @@ public class RecipeController {
         this.recipeService = recipeService;
     }
 
+    @GetMapping
     @RequestMapping("/recipe/{id}/show")
     public String showById(@PathVariable String id, Model model){
         model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
