1b23464aa880cf3f7607ae9a1067a6d92e859f25
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2019-08-24 09:52:33 +0530

    233 - Color Logging

--- src/main/java/guru/springframework/services/RecipeServiceImpl.java
+++ src/main/java/guru/springframework/services/RecipeServiceImpl.java
@@ -47,7 +47,7 @@ public class RecipeServiceImpl implements RecipeService {
 
         if (!recipeOptional.isPresent()) {
             //throw new RuntimeException("Recipe Not Found!");
-            throw new NotFoundException("Recipe Not Found");
+            throw new NotFoundException("Recipe: " + l.toString() + " Not Found");
         }
 
         return recipeOptional.get();
